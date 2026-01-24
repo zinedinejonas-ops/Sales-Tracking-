d069d3b20ae54562d3becb9220fd308d98ba1f57
Zinedine Jonas Kasambala <zinedinejonas@gmail.com>
2026-01-24 23:08:49 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -41,7 +41,7 @@ app.get('/health', (req, res) => {
   res.json({ ok: true })
 })
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 1000
 app.listen(port, async () => {
   console.log(`Server listening on port ${port}`)
   try {
